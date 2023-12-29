de4a80a3e79c5162b2d66a9455cca1151a1d93b8
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2023-12-29 15:38:04 +0200

    add HttpError.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const contacts = require("../../models/contacts");
 
+const { HttpError } = require("../../helpers");
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
@@ -19,9 +21,15 @@ router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
+
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+
     res.json(result);
   } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
   }
 });
 
