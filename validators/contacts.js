e15162f44efdbe0066248d32b4d575b59cbe3947
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2023-12-30 09:21:18 +0200

    update createContactSchema (messages)

--- validators/contacts.js
+++ validators/contacts.js
@@ -1,11 +1,25 @@
 const Joi = require("joi");
 
 const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
+  name: Joi.string().required().messages({
+    "any.required": "Name is required.",
+    "string.empty": "The name cannot be empty",
+  }),
+
+  email: Joi.string().email().required().messages({
+    "string.empty": "The email cannot be empty",
+    "string.email": "Invalid email format.",
+    "any.required": "Email is required. ",
+  }),
+
   phone: Joi.string()
     .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .required(),
+    .required()
+    .messages({
+      "string.empty": "The phone number cannot be empty",
+      "string.pattern.base": "Invalid phone number format.",
+      "any.required": "Phone number is required.",
+    }),
 });
 
 module.exports = {
