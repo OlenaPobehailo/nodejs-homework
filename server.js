25e30ebf95fc4e864739e4c6d87c4fbfc6d24734
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-01-01 14:38:49 +0200

    create a connection to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Olena:p6offVzrvBFjDWzT@cluster0.bd2pbdt.mongodb.net/";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(
+        "Server is running. Database connection successful. Use our API on port: 3000."
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
