5a8e5e523b269c10c610d09164becb4afe54f307
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-01-04 19:43:07 +0200

    add pagination for the collection of contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,8 +4,13 @@ const { ctrlWrapper } = require("../decorators");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email subscription");
 
   res.json(result);
 };
