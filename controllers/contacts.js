7677a8d77ce887b1b02e6a34ed5f72da5ed68070
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-01-04 20:26:53 +0200

    filter contacts by favorite field (GET /contacts?favorite=true)

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,10 +4,12 @@ const { ctrlWrapper } = require("../decorators");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  const filter = { owner, favorite: favorite === "true" };
+
+  const result = await Contact.find(filter, "-createdAt -updatedAt", {
     skip,
     limit,
   }).populate("owner", "email subscription");
